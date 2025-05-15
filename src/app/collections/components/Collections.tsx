8645bee846640831dcdc6669b44115c4c6d53bb5
heshuai <13752727326@163.com>
2025-05-15 15:19:33 +0800

    开发nft

--- src/app/collections/components/Collections.tsx
+++ src/app/collections/components/Collections.tsx
@@ -1,10 +1,42 @@
 'use client'
+import CollectionsTable from "./CollectionsTab";
+import collectionApi from "@/api/collections";
+import { useEffect, useState } from "react";
+import useGlobalState from "@/hooks/useGlobalState";
+import { useAccount } from "wagmi";
 
+const Collections = ({type}: {type: string}) => {
+    const [collections, setCollections] = useState<any[]>([]);
+    const [loading, setLoading] = useState(false);
+    const { state, setState } = useGlobalState();
+    const { address } = useAccount();
 
-const Collections = () => {
+    useEffect(() => {
+        if (address) {
+          setState({ walletAddress: address as string });
+        }
+    }, [address]);
+
+    async function fetchCollections() {
+        setLoading(true);
+         
+        // if (type == "trending") {
+        const res = await collectionApi.GetCollections({
+            limit: 10,
+            range: "1d",
+        });
+        // @ts-ignore
+        setCollections(res?.result || []);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchCollections();
+    }, []);
+
     return (
         <div>
-
+            <CollectionsTable collections={collections} loading={loading} />
         </div>
     )
 }
